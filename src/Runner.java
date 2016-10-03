d9cfc5ddd9f9dd54e8ab646f9fb314e88852f5d2
denisoetken <denisoetken@hotmail.de>
2016-10-03 18:56:01 +0200

    Zugfahrt_IC_2435

--- src/Runner.java
+++ src/Runner.java
@@ -3,6 +3,23 @@
  */
 public class Runner {
     public static void main(String[] args) {
-
+        Names testPerson = new Names();
+        System.out.println("Vornamen: ");
+        System.out.println(testPerson.vornamenErzeugen());
+        System.out.println(testPerson.vornamenErzeugen());
+        System.out.println(testPerson.vornamenErzeugen());
+        System.out.println("Nachname: ");
+        System.out.println(testPerson.nachNamenErzeugen());
+        System.out.println(testPerson.nachNamenErzeugen());
+        System.out.println(testPerson.nachNamenErzeugen());
+        System.out.println("Wohnorte: ");
+        System.out.println(testPerson.wohnOrtErzeugen());
+        System.out.println(testPerson.wohnOrtErzeugen());
+        System.out.println("Strasse: ");
+        System.out.println(testPerson.strasseErzeugen());
+        System.out.println(testPerson.strasseErzeugen());
+        System.out.println("Hausnummer: ");
+        System.out.println(testPerson.hausNummerErzeugen());
+        System.out.println(testPerson.hausNummerErzeugen());
     }
 }
