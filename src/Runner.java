e145f92d7547c43ffa05bd5ea8a0d45a3b588d6a
denisoetken <denisoetken@hotmail.de>
2016-10-04 16:15:10 +0200

    Generator/Person

--- src/Runner.java
+++ src/Runner.java
@@ -12,14 +12,7 @@ public class Runner {
     public static void main(String[] args) {
         int anzahl = 10;
         Person p = new Person();
-        //     StringBuilder sb = new StringBuilder();
-        //       BufferedWriter wr = null;
 
-
-        //   for (String h : p.anzahlSchleife(anzahl)) {
-        //     sb.append(h);
-        //   sb.append("\n");
-        //}
         try (BufferedWriter wr = new BufferedWriter(new FileWriter("test.txt"))) {
             for (String s : p.anzahlSchleife(anzahl)) {
                 wr.append(s);
